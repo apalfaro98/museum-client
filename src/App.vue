47792cc3ff7d28cc0ba44a36f4fd5cb8cd26e4f8
Angelica Perez <apalfaro9803@gmail.com>
2022-10-06 11:22:17 -0400

    ADD: menu de la primera pagina

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,91 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="light-blue"
-      dark
-      dense
-    > 
-    <v-spacer></v-spacer> 
-    <!-- FIXME: EL icono no cambia cuando se da click fuera del boton  -->
-    <v-menu
-      transition="slide-y-transition"
-      bottom
-      offset-y
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="black"
-          text
-          plain
-          class="font-weight-bold"
-          v-bind="attrs"
-          v-on="on"
-          @click="changeIcon"
-        >
-          Secciones
-          <v-icon>mdi-menu-{{ down ? 'down' : 'up'}}</v-icon>
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>  
-    <v-btn
-      color="black"
-      text
-      plain
-      class="font-weight-bold"
-    >
-      <v-icon class="pr-2">mdi-account-lock</v-icon>
-      Administrar
-    </v-btn> 
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+    <v-app>
+        <v-app-bar app color="light-blue" dark dense>
+            <router-link to="/">
+                <div class="image-container">
+                    <v-img src="@/assets/images/logo-small.png" />
+                </div>
+            </router-link>
+            <v-spacer></v-spacer>
+            <router-link to="/prestamos">
+                <v-btn color="black" text plain class="font-weight-bold">
+                    <v-icon class="pr-2">mdi-hand-coin</v-icon>
+                    Prestamos
+                </v-btn>
+            </router-link>
+            <!-- FIXME: EL icono no cambia cuando se da click fuera del boton  -->
+            <v-menu
+                transition="slide-y-transition"
+                bottom
+                offset-y
+                rounded="br-xl tl-xl"
+            >
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        color="black"
+                        text
+                        plain
+                        class="font-weight-bold"
+                        v-bind="attrs"
+                        v-on="on"
+                        @click="changeIcon"
+                    >
+                        <v-icon class="pr-2">mdi-file-document</v-icon>
+                        Secciones
+                        <v-icon>mdi-menu-{{ down ? 'down' : 'up' }}</v-icon>
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item v-for="(item, i) in items" :key="i">
+                        <router-link
+                            :to="item.route"
+                            class="grey--text text--darken-4"
+                        >
+                            <v-list-item-title @click="changeIcon">
+                                {{ item.title }}
+                            </v-list-item-title>
+                        </router-link>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+            <router-link to="/login">
+                <v-btn color="black" text plain class="font-weight-bold">
+                    <v-icon class="pr-2">mdi-account-lock</v-icon>
+                    Administrar
+                </v-btn>
+            </router-link>
+        </v-app-bar>
+        <v-main>
+            <router-view />
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+    name: 'App',
 
-  data: () => ({
-    items: [
-        { title: 'ARMAS' },
-        { title: 'CIENCIAS NATURALES' },
-      ],
-      down: true,
-  }),
-  methods: {
-    changeIcon() {
-      this.down = !this.down;
-    }
-  }
+    data: () => ({
+        items: [
+            { title: 'ARMAS', route: '/armas' },
+            { title: 'CIENCIAS NATURALES', route: '/naturales' },
+        ],
+        down: true,
+    }),
+    methods: {
+        changeIcon() {
+            this.down = !this.down;
+        },
+    },
 };
 </script>
+
+<style scoped>
+a {
+    text-decoration: none;
+}
+.image-container {
+    width: 27px;
+    height: 27px;
+}
+</style>
