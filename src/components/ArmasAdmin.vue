9b18e2fda5a9f49581db9c9cab4ace077965cc82
Angelica Perez <apalfaro9803@gmail.com>
2022-10-11 14:55:32 -0400

    ADD: components for admin

---
+++ src/components/ArmasAdmin.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Armas</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,66 +1,84 @@
 <template>
-    <v-card>
-        <v-navigation-drawer
-            bottom
-            mobile-breakpoint="768"
-            fixed
-            class="back pt-16"
-            expand-on-hover
-        >
-            <v-list nav dense>
-                <v-list-item-group v-model="selectedItem" color="primary">
-                    <v-list-item
-                        v-for="(item, i) in items"
-                        :key="i"
-                        @click="selectedSection = -1"
-                    >
-                        <v-list-item-icon>
-                            <v-icon v-text="item.icon"></v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                            <v-list-item-title
-                                v-text="item.text"
-                            ></v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
-                </v-list-item-group>
-                <v-list-group
-                    :value="true"
-                    prepend-icon="mdi-file-document"
-                    no-action
-                >
-                    <template v-slot:activator>
-                        <v-list-item-title>Secciones</v-list-item-title>
-                    </template>
-                    <v-list-item-group
-                        class="pl-6"
-                        v-model="selectedSection"
-                        color="primary"
-                    >
+    <div>
+        <v-card>
+            <v-navigation-drawer
+                bottom
+                mobile-breakpoint="768"
+                fixed
+                class="back pt-16"
+                expand-on-hover
+            >
+                <v-list nav dense>
+                    <v-list-item-group v-model="selectedItem" color="primary">
                         <v-list-item
-                            v-for="(item, i) in sections"
+                            v-for="(item, i) in items"
                             :key="i"
-                            link
-                            @click="selectedItem = -1"
+                            @click="selectedSection = -1"
                         >
+                            <v-list-item-icon>
+                                <v-icon v-text="item.icon"></v-icon>
+                            </v-list-item-icon>
                             <v-list-item-content>
                                 <v-list-item-title
                                     v-text="item.text"
                                 ></v-list-item-title>
                             </v-list-item-content>
-                            <v-list-item-icon>
-                                <v-icon v-text="item.icon"></v-icon>
-                            </v-list-item-icon>
                         </v-list-item>
                     </v-list-item-group>
-                </v-list-group>
-            </v-list>
-        </v-navigation-drawer>
-    </v-card>
+                    <v-list-group
+                        :value="true"
+                        prepend-icon="mdi-file-document"
+                        no-action
+                    >
+                        <template v-slot:activator>
+                            <v-list-item-title>Secciones</v-list-item-title>
+                        </template>
+                        <v-list-item-group
+                            class="pl-6"
+                            v-model="selectedSection"
+                            color="primary"
+                        >
+                            <v-list-item
+                                v-for="(item, i) in sections"
+                                :key="i"
+                                link
+                                @click="selectedItem = -1"
+                            >
+                                <v-list-item-content>
+                                    <v-list-item-title
+                                        v-text="item.text"
+                                    ></v-list-item-title>
+                                </v-list-item-content>
+                                <v-list-item-icon>
+                                    <v-icon v-text="item.icon"></v-icon>
+                                </v-list-item-icon>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list-group>
+                </v-list>
+            </v-navigation-drawer>
+        </v-card>
+        <div class="ml-16">
+            <estadistica-admin v-if="selectedItem === 0" />
+            <prestamos-admin v-else-if="selectedItem === 1" />
+            <armas-admin v-if="selectedSection === 0" />
+            <naturales-admin v-else-if="selectedSection === 1" />
+        </div>
+    </div>
 </template>
 
 <script>
+import EstadisticaAdmin from '@/components/EstadisticaAdmin.vue';
+import PrestamosAdmin from '@/components/PrestamosAdmin.vue';
+import ArmasAdmin from '@/components/ArmasAdmin.vue';
+import NaturalesAdmin from '@/components/NaturalesAdmin.vue';
 export default {
+    components: {
+        EstadisticaAdmin,
+        PrestamosAdmin,
+        ArmasAdmin,
+        NaturalesAdmin,
+    },
     data: () => ({
         selectedItem: 0,
         selectedSection: -1,
