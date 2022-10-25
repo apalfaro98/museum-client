7d4addf75ade21ffcd9b59ae752bfb633b76b4cd
Angelica Perez <apalfaro9803@gmail.com>
2022-10-25 17:15:00 -0400

    FIX: Problem with token

--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000/api';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+const museoApi = axios.create({
+    baseURL: 'http://localhost:3000/api',
+});
 
-if (localStorage.getItem('token')) {
-    axios.defaults.headers.common['Authorization'] =
-        localStorage.getItem('token');
-}
+museoApi.interceptors.request.use((config) => {
+    config.headers['Authorization'] = localStorage.getItem('token');
+
+    return config;
+});
 
 const auth = async (name = '', password = '') => {
-    const resp = await axios.post('/auth', {
+    const resp = await museoApi.post('/auth', {
         name,
         password,
     });
@@ -18,7 +20,7 @@ const auth = async (name = '', password = '') => {
 };
 
 const checkLogued = async (token = '') => {
-    const resp = await axios.post('/auth/lookup', {
+    const resp = await museoApi.post('/auth/lookup', {
         token,
     });
     const data = resp.data;
@@ -26,67 +28,67 @@ const checkLogued = async (token = '') => {
 };
 
 const getEstadistica = async () => {
-    const resp = await axios.get('/estadistica');
+    const resp = await museoApi.get('/estadistica');
     const data = resp.data;
     return data;
 };
 
 const getArmas = async () => {
-    const resp = await axios.get('/armas');
+    const resp = await museoApi.get('/armas');
     const data = resp.data;
     return data;
 };
 
 const getNaturales = async () => {
-    const resp = await axios.get('/naturales');
+    const resp = await museoApi.get('/naturales');
     const data = resp.data;
     return data;
 };
 
 const getPrestamos = async () => {
-    const resp = await axios.get('/prestamos');
+    const resp = await museoApi.get('/prestamos');
     const data = resp.data;
     return data;
 };
 
 const getDonaciones = async () => {
-    const resp = await axios.get('/donaciones');
+    const resp = await museoApi.get('/donaciones');
     const data = resp.data;
     return data;
 };
 
 const getBajas = async () => {
-    const resp = await axios.get('/bajas');
+    const resp = await museoApi.get('/bajas');
     const data = resp.data;
     return data;
 };
 
 const getOneArma = async (id) => {
-    const resp = await axios.get(`/armas/${id}`);
+    const resp = await museoApi.get(`/armas/${id}`);
     const data = resp.data;
     return data;
 };
 
 const getOneNatural = async (id) => {
-    const resp = await axios.get(`/naturales/${id}`);
+    const resp = await museoApi.get(`/naturales/${id}`);
     const data = resp.data;
     return data;
 };
 
 const getOnePrestamo = async (id) => {
-    const resp = await axios.get(`/prestamos/${id}`);
+    const resp = await museoApi.get(`/prestamos/${id}`);
     const data = resp.data;
     return data;
 };
 
 const getOneDonacion = async (id) => {
-    const resp = await axios.get(`/donaciones/${id}`);
+    const resp = await museoApi.get(`/donaciones/${id}`);
     const data = resp.data;
     return data;
 };
 
 const createArma = async (arma = {}, image) => {
-    const resp = await axios.post(
+    const resp = await museoApi.post(
         '/armas',
         {
             ...arma,
@@ -103,7 +105,7 @@ const createArma = async (arma = {}, image) => {
 };
 
 const updateArma = async (arma = {}) => {
-    const resp = await axios.put(`/armas/${arma._id}`, {
+    const resp = await museoApi.put(`/armas/${arma._id}`, {
         ...arma,
     });
     const data = resp.data;
@@ -111,12 +113,12 @@ const updateArma = async (arma = {}) => {
 };
 
 const deleteArma = async (id) => {
-    const resp = await axios.delete(`/armas/${id}`);
+    const resp = await museoApi.delete(`/armas/${id}`);
     const data = resp.data;
     return data;
 };
 const createNatural = async (natural = {}, image) => {
-    const resp = await axios.post(
+    const resp = await museoApi.post(
         '/naturales',
         {
             ...natural,
@@ -133,7 +135,7 @@ const createNatural = async (natural = {}, image) => {
 };
 
 const updateNatural = async (natural = {}) => {
-    const resp = await axios.put(`/naturales/${natural._id}`, {
+    const resp = await museoApi.put(`/naturales/${natural._id}`, {
         ...natural,
     });
     const data = resp.data;
@@ -141,47 +143,47 @@ const updateNatural = async (natural = {}) => {
 };
 
 const deleteNatural = async (id) => {
-    const resp = await axios.delete(`/naturales/${id}`);
+    const resp = await museoApi.delete(`/naturales/${id}`);
     const data = resp.data;
     return data;
 };
 const createPrestamo = async (prestamo = {}) => {
-    const resp = await axios.post('/prestamos', prestamo);
+    const resp = await museoApi.post('/prestamos', prestamo);
     const data = resp.data;
     return data;
 };
 
 const updatePrestamo = async (prestamo = {}) => {
-    const resp = await axios.put(`/prestamos/${prestamo._id}`, prestamo);
+    const resp = await museoApi.put(`/prestamos/${prestamo._id}`, prestamo);
     const data = resp.data;
     return data;
 };
 
 const deletePrestamo = async (id) => {
-    const resp = await axios.delete(`/prestamos/${id}`);
+    const resp = await museoApi.delete(`/prestamos/${id}`);
     const data = resp.data;
     return data;
 };
 
 const getTransferencias = async () => {
-    const resp = await axios.get('/transferencias');
+    const resp = await museoApi.get('/transferencias');
     const data = resp.data;
     return data;
 };
 const getOneTransferencia = async (id) => {
-    const resp = await axios.get(`/transferencias/${id}`);
+    const resp = await museoApi.get(`/transferencias/${id}`);
     const data = resp.data;
     return data;
 };
 
 const createTransferencia = async (transferencia = {}) => {
-    const resp = await axios.post('/transferencias', transferencia);
+    const resp = await museoApi.post('/transferencias', transferencia);
     const data = resp.data;
     return data;
 };
 
 const createDonacion = async (donacion = {}) => {
-    const resp = await axios.post('/donaciones', donacion);
+    const resp = await museoApi.post('/donaciones', donacion);
     const data = resp.data;
     return data;
 };
